14b35d6469c6f5aa27c86560b26f7b4957269e39
mugeshi <nicolemugeshi@gmail.com>
2023-09-29 12:53:39 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
@@ -29,6 +29,36 @@ def index_articles():
 def show_article(id):
 
     pass
+
+@app.route('/articles/<int:id>', methods=['GET'])
+def show_article_details(id):
+    #check if 'page_views' is in the session, if not, set it to 0
+    session['page_views'] = session.get('page_views', 0)
+
+    #increment the 'page_views' count  for each request
+    session['page_views'] += 1
+    print(f"page_views: {session['page_views']}")
+
+    #check if the user has viewd more than 3 pages
+    if session['page_views'] > 3 :
+        #Return an unauthorized(401) response
+        return jsonify({'message': 'Maximum pageview limit reached '}), 401
+    
+    #if the user has viewd 3 or fewer pages, return the articles data(replace this with your actual data)
+    article_data = {
+        'id': id,
+        'title': 'sample Article',
+        'content': 'This is a sample article content.'
+ }
+    # Return a valid response with status code 200 (OK)
+    return jsonify({'article': article_data}), 200
+    
+    
+    return jsonify({'article': article_data, 'page_views': session['page_views']})
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
